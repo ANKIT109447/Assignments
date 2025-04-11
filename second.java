bec54521da1c07561bc42921066ac99815c57fe3
Your Name <your_email@example.com>
2025-04-11 21:39:40 +0530

    second commit

---
+++ second.java
@@ -0,0 +1,14 @@
+public class second {
+    public static void main(String[]args){
+        int a=5;
+        float b=6;
+        double c=7;
+        char d='a';
+        boolean flag=true;
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+        System.out.println(d);
+        System.out.println(flag);
+}
+}
