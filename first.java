6c06191e9fc2b53de15dbe8e843ff3e70d05e933
Your Name <your_email@example.com>
2025-04-11 21:33:35 +0530

    first commit

---
+++ first.java
@@ -0,0 +1,9 @@
+public class first{
+    static int a=5;
+    int b=6;
+    public static void main(String[]args){
+        System.out.println(a);
+        first obj =new first();
+        System.out.print(obj.b);
+    }
+}
